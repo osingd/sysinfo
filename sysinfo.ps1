04657dc59fd98b9abc9af526d69800ad2e4a5f3c
SOFTSERVE\ybend <ybend@softserveinc.com>
2025-01-15 16:54:56 +0200

    sysinfo was modified

--- sysinfo.ps1
+++ sysinfo.ps1
@@ -1,13 +1,10 @@
-mkdir c:\buildArtifacts
-echo Azure-Image-Builder-Was-Here  > c:\buildArtifacts\systeminfo.txt
-
+$outputFile = "c:\buildArtifacts\systeminfo.txt"
 $currentDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 $hostname = $env:COMPUTERNAME
 $cpuName = (Get-CimInstance -ClassName Win32_Processor).Name
 $ramSize = "{0:N2} GB" -f ((Get-CimInstance -ClassName Win32_ComputerSystem).TotalPhysicalMemory / 1GB)
 $diskSize = (Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | Measure-Object -Property Size -Sum).Sum / 1GB
 $ipAddress = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -notlike "Loopback"}).IPAddress -join ", "
-
 $output = @"
 Date: $currentDate
 Hostname: $hostname
@@ -17,7 +14,9 @@ Disk size: $("{0:N2} GB" -f $diskSize)
 Ip address: $ipAddress
 "@
 
-$outputFile = "c:\buildArtifacts\systeminfo.txt"
+mkdir c:\buildArtifacts
+echo Azure-Image-Builder-Was-Here  > c:\buildArtifacts\systeminfo.txt
+
 $output | Out-File -FilePath $outputFile -Encoding UTF8
 
 Write-Host "All information is saved in the file: $outputFile"
