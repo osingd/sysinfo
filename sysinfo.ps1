86b7f166e40931091d0a2901be5380a079a644e9
SOFTSERVE\ybend <ybend@softserveinc.com>
2025-01-15 03:07:24 +0200

    initial

---
+++ sysinfo.ps1
@@ -0,0 +1,20 @@
+$currentDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$hostname = $env:COMPUTERNAME
+$cpuName = (Get-CimInstance -ClassName Win32_Processor).Name
+$ramSize = "{0:N2} GB" -f ((Get-CimInstance -ClassName Win32_ComputerSystem).TotalPhysicalMemory / 1GB)
+$diskSize = (Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | Measure-Object -Property Size -Sum).Sum / 1GB
+$ipAddress = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -notlike "Loopback"}).IPAddress -join ", "
+
+$output = @"
+Date: $currentDate
+Hostname: $hostname
+Cpu name: $cpuName
+Memory size: $ramSize
+Disk size: $("{0:N2} GB" -f $diskSize)
+Ip address: $ipAddress
+"@
+
+$outputFile = "$env:USERPROFILE\Desktop\SystemInfo.txt"
+$output | Out-File -FilePath $outputFile -Encoding UTF8
+
+Write-Host "All information is saved in the file: $outputFile"
